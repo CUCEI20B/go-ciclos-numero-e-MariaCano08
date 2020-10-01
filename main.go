5b11b5d5bb83bd5418e1a72b6a753fa798df782f
María Cano <maria.cano4435@alumnos.udg.mx>
2020-09-30 21:24:50 -0500

    Programa de euler 2

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var e, fac, i, j float64;
-	e=0;
-	for i = 1; i<1000; i++ {
+	var e, fac, i, j, a float64
+	e = 0
+	fmt.Scan(&a)
+	for i = 1; i < a; i++ {
 		fac = 1
 		for j = 1; j < i; j++ {
 			fac = fac * j
 		}
 		//.Println(fac);
-		e =e+ (1 / fac)
+		e = e + (1 / fac)
 	}
 	fmt.Println(e)
 }
